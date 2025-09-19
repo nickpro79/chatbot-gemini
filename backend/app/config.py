e04962fdc7db7f20ebde4f3cdaf76f23cc3b779b
Nikhil <nikhilpraveen7777@gmail.com>
2025-09-19 19:16:26 +0530

    created config.py

---
+++ backend/app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("❌ GEMINI_API_KEY not found in .env file")
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+
+gemini_model = genai.GenerativeModel("gemini-pro")
